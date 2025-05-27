65672b4c4bbb7ebd30e6a0923fafe318f998c0a2
kishor2581 <privatework278@gmail.com>
2025-05-27 11:54:54 +0530

    saved

---
+++ Rectangle.java
@@ -0,0 +1,29 @@
+package rectangle;
+
+// Rectangle.java
+public class Rectangle {
+    // Attributes
+    double length;
+    double breadth;
+
+    // Constructor to initialize values
+    public Rectangle(double len, double brd) {
+        length = len;
+        breadth = brd;
+    }
+
+    // Method to calculate and return area
+    public double calculateArea() {
+        return length * breadth;
+    }
+
+    // Main method to test the class
+    public static void main(String[] args) {
+        // Creating a Rectangle object
+        Rectangle rect1 = new Rectangle(5.0, 3.0);
+
+        // Calculating and displaying the area
+        double area = rect1.calculateArea();
+        System.out.println("Area of the Rectangle: " + area);
+    }
+}
